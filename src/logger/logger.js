7d0dbd6deb1cfc363fb8fd1039558f0751a01943
Sagarpatel14 <sp43273@gmail.com>
2022-06-02 11:33:21 +0530

    assignment module system

---
+++ src/logger/logger.js
@@ -0,0 +1,4 @@
+const welcome = function(){
+    console.log("I am Sagar and a part of FunctionUp Radon cohort")
+}
+module.exports.welcome = welcome
---
+++ src/validator/formatter.js
@@ -0,0 +1,15 @@
+let trimIt = function(){
+    const d =  "    functionup    ".trim();
+    console.log(d);
+}
+let changetoLowerCase = function(){
+    const e = " SAGAR PATEL".toLowerCase();
+    console.log(e);
+}
+let changetoUpperCase = function(){
+    const f = "sagar patel".toUpperCase();
+    console.log(f);
+}
+module.exports.trimIt = trimIt
+module.exports.changetoLowerCase = changetoLowerCase
+module.exports.changetoUpperCase = changetoUpperCase
