943a798e518ca78bdcf0e81c53c0cd80945c9cb6
Sagarpatel14 <sp43273@gmail.com>
2022-06-07 10:13:33 +0530

    this is a assignment in mongo session-02

---
+++ src/controller/controller.js
@@ -0,0 +1,68 @@
+const bookModel = require('../model/bookModel')
+
+
+
+const creatBook = async function(req , res){
+    let Data = req.body;
+    let saveData = await bookModel.create(Data);
+    res.send({msg : saveData})
+}
+
+//=======================================solution:01============================================================
+
+
+const bookList = async function(req , res){
+    let allBooks = await bookModel.find().select({ bookName : 1 ,authorName : 1, _id : 0 });
+    res.send({msg : allBooks})
+}
+
+//=========================================solution:02============================================================
+
+
+const getBooksInYear = async function(req , res){
+    let myYear = req.body
+    let allBooks = await bookModel.find(myYear).select({bookName : 1, _id : 0});
+    res.send({msg : allBooks})
+}
+
+
+//============================================solution:03===========================================================
+
+
+
+const getParticularBooks = async function(req , res){
+    let myData = req.body
+    let allBooks = await bookModel.find(myData);
+    res.send({msg : allBooks})
+}
+
+
+//===========================================solution:04==========================================================
+
+
+
+const getXINRBooks = async function(req , res){
+    let allBooks = await bookModel.find({  "PriceodBOok.indianPrice"  : {$in : ["600","450","500"]}  }).select({bookName : 1, _id : 0});
+    res.send({msg : allBooks})
+}
+
+
+
+//=============================================solution:05========================================================
+
+
+
+const getRandomBooks = async function(req , res){
+    let allBooks = await bookModel.find({ $or : [{totalPages : {$gt : 500}},{stockAvailable : true}]}).select({bookName : 1, _id : 0}).count()
+    res.send({msg : allBooks})
+}
+
+//=================================solution:06=======================================================================
+
+
+module.exports.creatBook = creatBook;
+module.exports.bookList = bookList;
+module.exports.getBooksInYear = getBooksInYear;
+module.exports.getXINRBooks = getXINRBooks;
+module.exports.getRandomBooks = getRandomBooks;
+module.exports.getParticularBooks = getParticularBooks;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,36 +1,20 @@
 const express = require('express');
-// const externalModule = require('./logger')
-const lodash = require('lodash')
-
 const router = express.Router();
-
-router.get('/hello', function (req, res) {
-   let myArr1 = ["january","february","march","april","may","june","july","august","september","october","november","december"]
-    let sepArrays = lodash.chunk(myArr1,3)
-    console.log(sepArrays)
-
-    
-
-    let myArr = lodash.tail([1,3,5,7,9,11,13,15,17,19])
-    console.log(`the tail of the array is ${myArr}`)
+const bookModel = require('../model/bookModel.js');
+const Controller = require('../controller/Controller.js');
 
 
+router.post('/creatBook' , Controller.creatBook )
 
-    let unionArr = lodash.union([1,2],[2,3],[3,4],[5,6],[6,7,8])
-    console.log("the union of the array without repeatation is "+ unionArr)
+router.get('/bookList', Controller.bookList)
 
+router.post('/getBooksInYear', Controller.getBooksInYear)
 
+router.get('/getXINRBooks', Controller.getXINRBooks)
 
-    let movieLib = lodash.fromPairs([["horror","conjuring"],["loveStory","Titanic"],["Thriller","strangerThings"],["best_movie","SardarUdhamSingh"]])
-    console.log("the object of the movie library is " + JSON.stringify(movieLib))
-
-
-    res.send('Hello there!')
+router.get('/getRandomBooks', Controller.getRandomBooks)
 
 
-});
-
-
+router.post('/getParticularBooks', Controller.getParticularBooks)
 
 module.exports = router;
-// adding this comment for no reason
