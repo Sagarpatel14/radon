4b2cc718926069ef6b1b563bb3c31556f617cee2
Sagarpatel14 <sp43273@gmail.com>
2022-06-10 23:15:28 +0530

    Assignmnet on middlewares-01

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 var bodyParser = require('body-parser');
+const ip = require("ip")
+const moment = require("moment")
 
 const route = require('./routes/route.js');
 
@@ -9,6 +11,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', route);
+
+app.use(function (req, res, next){
+    currentDateAndTime = moment().format("YYYY-MM-DD hh:MM:SS");
+    currentIp = req.ip;
+    currentReqestPath = req.path; 
+    console.log(currentDateAndTime + "," + currentIp + "," + currentReqestPath )
+})
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
